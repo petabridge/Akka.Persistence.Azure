9bcd6d897b43500f4b76a6aced485290138e5549
Aaron Stannard <aaron@petabridge.com>
2018-07-06 13:30:44 -0500

    cleaning

--- src/Akka.Persistence.Azure/Journal/AzureStreamingStorageJournal.cs
+++ src/Akka.Persistence.Azure/Journal/AzureStreamingStorageJournal.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Akka.Actor;
@@ -8,8 +10,10 @@ using Akka.Persistence.Journal;
 using Akka.Streams;
 using Akka.Streams.Dsl;
 using Akka.Streams.Implementation.Fusing;
+using Akka.Util.Internal;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
+using Debug = System.Diagnostics.Debug;
 
 namespace Akka.Persistence.Azure.Journal
 {
@@ -77,12 +81,12 @@ namespace Akka.Persistence.Azure.Journal
 
         private void HandleReplay(ReplayMessages replay)
         {
-            
+
         }
 
         private void HandleWrite(WriteMessages write)
         {
-            
+
         }
 
         protected override void PreStart()
@@ -94,89 +98,108 @@ namespace Akka.Persistence.Azure.Journal
 
             _log.Debug("Successfully started Azure Table Storage!");
 
-            _resequencer = Context.ActorOf(Props.Create(() => new Resequencer()));
-
             // need to call the base in order to ensure Akka.Persistence starts up correctly
             base.PreStart();
         }
 
-        private static IActorRef CreateWriteStream(IActorRefFactory factory, IActorRef resequencer)
+        private IActorRef CreateWriteStream()
         {
-            var source = Source.ActorRef<WriteWithCounter>(10000, OverflowStrategy.Fail);
-            
-        }
-
-        internal sealed class WriteWithCounter
-        {
-            public WriteWithCounter(AtomicWrite write, long sequenceCounterNumber)
-            {
-                Write = write;
-                SequenceCounterNumber = sequenceCounterNumber;
-            }
-
-            public AtomicWrite Write { get; }
-
-            public long SequenceCounterNumber { get; }
-        }
-
-        internal sealed class Desequenced
-        {
-            public Desequenced(object message, long sequenceNr, IActorRef target, IActorRef sender)
-            {
-                Message = message;
-                SequenceNr = sequenceNr;
-                Target = target;
-                Sender = sender;
-            }
-
-            public object Message { get; }
-
-            public long SequenceNr { get; }
-
-            public IActorRef Target { get; }
-
-            public IActorRef Sender { get; }
-        }
-
-        internal class Resequencer : ActorBase
-        {
-            private readonly Dictionary<long, Desequenced> _delayed = new Dictionary<long, Desequenced>();
-            private long _delivered = 0L;
-
-            protected override bool Receive(object message)
-            {
-                if (message is Desequenced d)
+            var source = Source.ActorRef<WriteMessages>(10000, OverflowStrategy.Fail);
+            var graph = source
+                .GroupedWithin(20, TimeSpan.FromMilliseconds(50))
+                .SelectAsync(10, writes =>
                 {
-                    do
+
+                })
+        }
+
+        protected async Task<IEnumerable<IJournalResponse>> WriteMessagesAsync(IEnumerable<WriteMessages> messages)
+        {
+            IJournalResponse summary = null;
+            var responses = new List<IJournalResponse>();
+
+ 
+#if DEBUG
+                _log.Debug("Entering method WriteMessagesAsync");
+#endif
+                var exceptions = ImmutableList<Exception>.Empty;
+                foreach (var atomicWrites in messages)
+                {
+                    
+                    foreach (var currentMsg in atomicWrites.Messages)
                     {
-                        d = Resequence(d);
-                    } while (d != null);
-                    return true;
+                        if (currentMsg is AtomicWrite x)
+                        {
+                            var successMsgs = new List<WriteMessageSuccess>();
+                            var batch = new TableBatchOperation();
+                            Debug.Assert(currentMsg != null, nameof(currentMsg) + " != null");
+                            foreach (var unadapted in (IImmutableList<IPersistentRepresentation>)x.Payload)
+                            {
+                                try
+                                {
+                                    var persistent = AdaptToJournal(unadapted);
+                                    batch.Insert(
+                                        new PersistentJournalEntry(persistent.PersistenceId,
+                                            persistent.SequenceNr, _serialization.PersistentToBytes(persistent),
+                                            persistent.Manifest));
+                                    
+                                    
+                                }
+                                catch (Exception ex)
+                                {
+                                    var failResp =
+                                        new WriteMessageRejected(unadapted, ex, atomicWrites.ActorInstanceId);
+                                    responses.Add(failResp);
+                                    continue;
+                                }
+
+                                var successResp = new WriteMessageSuccess(unadapted, atomicWrites.ActorInstanceId);
+                                successMsgs.Add(successResp);
+                            }
+
+                            if (_log.IsDebugEnabled && _settings.VerboseLogging)
+                                _log.Debug("Attempting to write batch of {0} messages to Azure storage", batch.Count);
+
+                            var results = await Table.ExecuteBatchAsync(batch);
+                            
+                            responses.AddRange(successMsgs);
+                        }
+                    }
+
+                    try
+                    {
+                       
+
+                        if (_log.IsDebugEnabled && _settings.VerboseLogging)
+                            foreach (var r in results)
+                                _log.Debug("Azure table storage wrote entity [{0}] with status code [{1}]", r.Etag,
+                                    r.HttpStatusCode);
+                    }
+                    catch (Exception ex)
+                    {
+                        exceptions = exceptions.Add(ex);
+                    }
                 }
-                return false;
+
+
+#if DEBUG
+                _log.Debug("Leaving method WriteMessagesAsync");
+                foreach (var ex in exceptions)
+                {
+                    _log.Error(ex, "recorded exception during write");
+                }
+#endif
+
+                /*
+                 * Part of the Akka.Persistence design.
+                 * 
+                 * Either return null or return an exception for each failed AtomicWrite.
+                 * 
+                 * Either everything fails or everything succeeds is the idea I guess.
+                 */
+                return exceptions.IsEmpty ? null : exceptions;
             }
-
-            private Desequenced Resequence(Desequenced desequenced)
-            {
-                if (desequenced.SequenceNr == _delivered + 1)
-                {
-                    _delivered = desequenced.SequenceNr;
-                    desequenced.Target.Tell(desequenced.Message, desequenced.Sender);
-                }
-                else
-                {
-                    _delayed.Add(desequenced.SequenceNr, desequenced);
-                }
-
-                var delivered = _delivered + 1;
-                if (_delayed.TryGetValue(delivered, out Desequenced d))
-                {
-                    _delayed.Remove(delivered);
-                    return d;
-                }
-
-                return null;
-            }
+   
         }
     }
 }
