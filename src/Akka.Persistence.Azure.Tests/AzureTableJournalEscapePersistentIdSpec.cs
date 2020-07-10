298e93ff1716d1ece9cd8976cc86ed0d8b17e28a
IgorFedchenko <igor@fedchenko.pro>
2020-07-10 20:29:06 +0300

    Spec with overriden Pid should fail

--- src/Akka.Persistence.Azure.Tests/AzureTableJournalEscapePersistentIdSpec.cs
+++ src/Akka.Persistence.Azure.Tests/AzureTableJournalEscapePersistentIdSpec.cs
@@ -1,23 +1,24 @@
+using System;
+using System.Reflection;
 using Akka.Actor;
+using Akka.Configuration;
+using Akka.Persistence.Azure.TestHelpers;
+using Akka.Persistence.TCK;
+using Akka.Persistence.TCK.Journal;
+using Akka.TestKit;
 using Xunit;
 using Xunit.Abstractions;
+using static Akka.Persistence.Azure.Tests.Helper.AzureStorageConfigHelper;
 
 namespace Akka.Persistence.Azure.Tests
 {
     public class AzureTableJournalEscapePersistentIdSpec : AzureTableJournalSpec
     {
-        /// <inheritdoc />
         public AzureTableJournalEscapePersistentIdSpec(ITestOutputHelper output) : base(output)
         {
-        }
-
-        [Fact]
-        public void Journal_should_replay_all_messages1()
-        {
-            var probe = CreateTestProbe();
-            Journal.Tell(new ReplayMessages(1, long.MaxValue, long.MaxValue, Pid, probe.Ref));
-            for (int i = 1; i <= 5; i++) probe.ExpectMsg<ReplayedMessage>(m => IsReplayedMessage(m, i));
-            probe.ExpectMsg<RecoverySuccess>();
+            var persistenceIdUsedForTests = typeof(PluginSpec).GetField($"<{nameof(Pid)}>k__BackingField", BindingFlags.Instance | BindingFlags.NonPublic);
+            var currentValue = persistenceIdUsedForTests.GetValue(this).ToString();
+            persistenceIdUsedForTests.SetValue(this, $"some/path/to/encode/{currentValue}");
         }
     }
 }
