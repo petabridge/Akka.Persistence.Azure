e618a25fd29a9da179964f67c2796168844d6649
IgorFedchenko <igor@fedchenko.pro>
2020-07-10 20:38:40 +0300

    Changed Pid overriding code

--- src/Akka.Persistence.Azure.Tests/AzureTableJournalEscapePersistentIdSpec.cs
+++ src/Akka.Persistence.Azure.Tests/AzureTableJournalEscapePersistentIdSpec.cs
@@ -16,6 +16,14 @@ namespace Akka.Persistence.Azure.Tests
     {
         public AzureTableJournalEscapePersistentIdSpec(ITestOutputHelper output) : base(output)
         {
+        }
+
+        /// <inheritdoc />
+        protected override void PreparePersistenceId(string pid)
+        {
+            base.PreparePersistenceId(pid);
+            
+            // Before storage is initialized, let's set Pid to the value that needs to be encoded
             var persistenceIdUsedForTests = typeof(PluginSpec).GetField($"<{nameof(Pid)}>k__BackingField", BindingFlags.Instance | BindingFlags.NonPublic);
             var currentValue = persistenceIdUsedForTests.GetValue(this).ToString();
             persistenceIdUsedForTests.SetValue(this, $"some/path/to/encode/{currentValue}");
