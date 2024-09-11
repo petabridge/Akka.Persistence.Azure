9ad5b7ad1194b4ae7d739e8c9907aafa5689fb3a
Gregorius Soedharmo <arkatufus@yahoo.com>
2024-09-12 02:16:26 +0700

    Implement SnapshotStore SaveSnapshot spec from TCK

---
+++ src/Akka.Persistence.Azure.Tests/AzureSnapshotStoreSaveSnapshotSpec.cs
@@ -0,0 +1,18 @@
+﻿using Akka.Persistence.TCK.Snapshot;
+using Xunit;
+using Xunit.Abstractions;
+using static Akka.Persistence.Azure.Tests.Helper.AzureStorageConfigHelper;
+
+namespace Akka.Persistence.Azure.Tests;
+
+
+[Collection("AzureSpecs")]
+public class AzureSnapshotStoreSaveSnapshotSpec: SnapshotStoreSaveSnapshotSpec
+{
+    public AzureSnapshotStoreSaveSnapshotSpec(ITestOutputHelper output) 
+        : base(AzureConfig(), nameof(AzureBlobSnapshotStoreSpec), output)
+    {
+        AzurePersistence.Get(Sys);
+    }
+    
+}
