241bf8cdebd260813cb75a69b099ab8c0779fd23
Gregorius Soedharmo <arkatufus@yahoo.com>
2024-09-12 00:43:35 +0700

    Fix port code to take DateTime.MinValue into account

--- src/Akka.Persistence.Azure/Snapshot/AzureBlobSnapshotStore.cs
+++ src/Akka.Persistence.Azure/Snapshot/AzureBlobSnapshotStore.cs
@@ -260,12 +260,20 @@ namespace Akka.Persistence.Azure.Snapshot
             cts.CancelAfter(_settings.RequestTimeout);
             using (cts)
             {
-                var response = await blobClient.GetPropertiesAsync(cancellationToken: cts.Token);
-                if (response.HasValue)
+                if (metadata.Timestamp == DateTime.MinValue)
                 {
-                    var timestamp = new DateTime(long.Parse(response.Value.Metadata[TimeStampMetaDataKey])); 
-                    if(timestamp <= metadata.Timestamp)
-                        await blobClient.DeleteAsync(cancellationToken: cts.Token);
+                    // Short-circuit the timestamp query if the metadata does not require us to check for timestamp
+                    await blobClient.DeleteIfExistsAsync(cancellationToken: cts.Token);
+                }
+                else
+                {
+                    var response = await blobClient.GetPropertiesAsync(cancellationToken: cts.Token);
+                    if (response.HasValue)
+                    {
+                        var timestamp = new DateTime(long.Parse(response.Value.Metadata[TimeStampMetaDataKey])); 
+                        if(timestamp <= metadata.Timestamp)
+                            await blobClient.DeleteAsync(cancellationToken: cts.Token);
+                    }
                 }
             }
         }
